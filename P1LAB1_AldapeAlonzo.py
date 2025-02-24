54b33997d77cf40a8d711d8b67bc0dc9bccfcd1a
Alonzo9209 <aldapea9209@student.faytechcc.edu>
2025-02-23 21:12:39 -0500

    Add files via upload
    first submission

---
+++ P1LAB1_AldapeAlonzo.py
@@ -0,0 +1,11 @@
+ # Alonzo Aldape
+ # 2/23/25
+ # P1LAB1
+ # Project is to make a program that lets you put in your name, and then it displays a welcome message
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name, "!", "Welcome to CTI-110")
+ 
